ff8b0c763d36b48d05bda99606ceaddb1465e346
Emily Gaska <emilygaska@gmail.com>
2020-05-18 20:59:31 -0500

    Completed test of program. Functioning. Updated readme and gitignore.

--- Develop/app.js
+++ Develop/app.js
@@ -26,6 +26,7 @@ function addTeam() {
             message: "What kind of employee would you like to add?",
             choices: ["Manager", "Engineer", "Intern"]
         }])
+        //adds the variable question based on employee type
         .then(function (data) {
             let varQuestion = "";
             if (data.employeeType === "Engineer") {
@@ -40,6 +41,7 @@ function addTeam() {
                 varQuestion = "school"
                 employeeType = "Intern";
             }
+            //prompt for employee information
             inquirer.prompt([
                 {
                     type: "input",
@@ -66,146 +68,40 @@ function addTeam() {
                     message: "Would you like to make another employee?",
                     name: "newMember"
                 }])
-                .then(function(data){
-                    if (employeeType === "Engineer"){
+                //Creates new employee and pushes it to the team array
+                .then(function (data) {
+                    if (employeeType === "Engineer") {
                         let newEmployee = new Engineer(data.name, data.id, data.email, data.varQuestion);
                         team.push(newEmployee);
                         console.log(team);
                     }
-                    else if(employeeType === "Manager") {
-                        let newEmployee = new Manager(data.name, data.id, data.email, data.officeNumber);
+                    else if (employeeType === "Manager") {
+                        let newEmployee = new Manager(data.name, data.id, data.email, data.varQuestion);
                         team.push(newEmployee);
                         console.log(team);
                     }
                     else {
-                        let newIntern = new Intern(data.name, data.id, data.email, data.officeNumber);
+                        let newIntern = new Intern(data.name, data.id, data.email, data.varQuestion);
                         team.push(newIntern);
                         console.log(team);
+                    }
+                    //If statement determines if the addTeam() runs again
+                    console.log(data.newMember);
+                    if (data.newMember === true) {
+                        console.log(team);
+                        addTeam();
+                    }
+                    else {
+                        console.log(team);
+                        createHTML(render(team));
                     }
 
                 })
         })
 }
 
-// function addAnother(){
-//     inquirer.prompt([
-//         {
-//             type: "confirm",
-//             name: "newEmployee",
-//             message: "Would you like to add another employee?"
-//         }
-//     ])
-//         .then(function (data) {
-//             if (data.newEmployee === true) {
-//                 addTeam();
-//             }
-//             else {
-//                 console.log(team);
-//             }
-//         })
-// }
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
-
-function engineerPrompts() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is the engineer's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is their ID?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is their email?"
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "What is their GitHub username?"
-        },
-    ])
-}
-
-function internPrompts() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is the intern's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is their ID?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is their email?"
-        },
-        {
-            type: "input",
-            name: "school",
-            message: "What school do they attend?"
-        }
-    ])
-};
-
-function managerPrompts() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is the manager's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is their ID?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is their email?"
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "What is their office number?"
-        }
-    ]).then(function (data) {
-        let newManager = new Manager(data.name, data.id, data.email, data.officeNumber);
-        team.push(newManager);
-        console.log(team);
-    })
-
+function createHTML(html) {
+        fs.mkdirSync(OUTPUT_DIR)
+    fs.writeFileSync(outputPath, html);
 }
 
